1c6e996432dbdd0a8cf74c32aaad4b69936f3a53
🐸 <dice-a6db@outlook.com>
2023-07-02 23:20:07 +0100

    support unsorted schemas, add trailing commas to dumps

--- public/dump.lua
+++ public/dump.lua
@@ -1,3 +1,10 @@
+--- Recursively generates a string representation of a Lua table.
+-- Handles tables with nested tables and prevents infinite recursion.
+-- The output is in a JSON-like format with commas.
+-- @param o The table to be dumped.
+-- @param seen An optional table keeping track of already visited tables to prevent infinite recursion.
+-- @param depth An optional parameter indicating current depth level for correct indentation.
+-- @return string A string representation of the table.
 function dump(o, seen, depth)
    seen = seen or {}
    depth = depth or 0
@@ -7,9 +14,19 @@ function dump(o, seen, depth)
       end
       seen[o] = true
       local parts = {}
+      local count = 0
       for k, v in pairs(o) do
+         count = count + 1
+      end
+      local i = 0
+      for k, v in pairs(o) do
+         i = i + 1
          local name = type(k) == 'number' and k or '"' .. k:gsub('"', '\\"') .. '"'
-         table.insert(parts, string.rep(" ", depth + 2) .. string.format("[%s] = %s", name, dump(v, seen, depth + 2, name), name))
+         if i == count then
+            table.insert(parts, string.rep(" ", depth + 2) .. string.format("[%s] = %s", name, dump(v, seen, depth + 2, name), name))
+         else
+            table.insert(parts, string.rep(" ", depth + 2) .. string.format("[%s] = %s,", name, dump(v, seen, depth + 2, name), name))
+         end
       end
       return "{\n" .. table.concat(parts, "\n") .. "\n" .. string.rep(" ", depth) .. "}"
    elseif type(o) == 'string' then
