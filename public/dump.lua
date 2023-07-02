02e6aa658d44f70d7b487f010788565751cb53e7
🐸 <dice-a6db@outlook.com>
2023-07-02 20:32:55 +0100

    add indentation to dumps

--- public/dump.lua
+++ public/dump.lua
@@ -1,13 +1,19 @@
-function dump(o)
+function dump(o, seen, depth)
+   seen = seen or {}
+   depth = depth or 0
    if type(o) == 'table' then
+      if seen[o] then
+         return "recursion"
+      end
+      seen[o] = true
       local parts = {}
       for k, v in pairs(o) do
-         k = type(k) == 'number' and k or '"' .. k .. '"'
-         table.insert(parts, string.format("[%s] = %s", k, dump(v)))
+         local name = type(k) == 'number' and k or '"' .. k:gsub('"', '\\"') .. '"'
+         table.insert(parts, string.rep(" ", depth + 2) .. string.format("[%s] = %s", name, dump(v, seen, depth + 2, name), name))
       end
-      return "{ " .. table.concat(parts, ", ") .. " }"
+      return "{\n" .. table.concat(parts, "\n") .. "\n" .. string.rep(" ", depth) .. "}"
    elseif type(o) == 'string' then
-      return '"' .. o .. '"'
+      return '"' .. o:gsub('"', '\\"'):gsub('\n', '\\n') .. '"'
    else -- for numbers and booleans
       return tostring(o)
    end
