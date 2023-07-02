b67847d5fec5d360af082d3c83cb6c9a97324df7
🐸 <dice-a6db@outlook.com>
2023-07-02 20:05:20 +0100

    remove tsc

--- src/hooks/useLuaEnv.ts
+++ src/hooks/useLuaEnv.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import * as fengari from "fengari-web";
 import { useAsync } from "react-use";
 
